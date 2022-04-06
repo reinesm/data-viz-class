f5edbd5b4bc35851b82103b5de8ac9394fd881b7
reinesmaliksi <rmaliksi@umd.edu>
2022-04-06 14:03:09 -0400

    pushing class practice code

---
+++ javascript.js
@@ -0,0 +1,48 @@
+// this is a comment
+
+/*
+Multine comment
+more lines and comments
+*/
+
+// alert("Hello World!")
+
+/*var amount = 5000;
+
+if (amount < 1000) {
+  alert("This is LESS than 1000");
+} else {
+  alert("This is MORE than 1000")
+};
+*/
+
+/*var headlineTwo = "New Headline";
+
+function newHeadline() {
+  document.getElementById("headline").innerHTML = headlineTwo;
+}
+*/
+/*
+Define variables
+var headlineArray = ["Next headline", "Another headline", "Yet Another headline,"
+"Final headline"];
+var headlineIndex = 0;
+var headlineLength = headlineArray.length;
+var myLoop = setInterval(changeHeadline, 1000);
+*/
+/*
+Function to loop through the headline headlineArray
+function changeHeadline() {
+  var headlineElement = document.getElementById("headline");
+  var newHeadline = headlineArray[headlineIndex];
+  //alert(newHeadline);
+  headlineElement.innerHTML = newHeadline;
+    headlineIndex++;
+  if (headlineIndex >= headlineLength) {
+    clearInterval(myLoop);
+  }
+}
+
+// Run the window
+window.onload = changeHeadline(myLoop);
+*/
